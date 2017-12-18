a03bc60bf398d3d45fc90389a229334a32cbbccc
stbe8715 <stefan.betzmeir@elektrobit.com>
2017-12-18 10:38:48 +0100

    Schwarze Balken hinzugefügt

--- main.lua
+++ main.lua
@@ -1,7 +1,8 @@
 -- Load some default values for our rectangle.
 function love.load()
-	
-	love.window.setMode(1280,720)
+	WIDTH = 1280
+	HEIGHT = 720
+	love.window.setMode(WIDTH,HEIGHT)
 	
     g_back = love.graphics.newImage( "gfx/back.png" )
 	g_top = love.graphics.newImage( "gfx/eye_top.png" )
@@ -15,17 +16,27 @@ end
  
 -- Draw a coloured rectangle.
 function love.draw()
+	love.graphics.setColor(255,255,255)
     love.graphics.draw(g_back,0,0)
 	
 	local mouseX, mouseY = love.mouse.getPosition()
-	drawLid(g_bottom,mouseX,mouseY,1.0,false)
-	drawLid(g_top,mouseX,mouseY,1.0,true)
+	local bottomX, bottomY = drawLid(g_bottom,mouseX,mouseY,1.0,false)
+	local topX, topY = drawLid(g_top,mouseX,mouseY,1.0,true)
+	
+	-- Draw bars to cover rest of background
+	love.graphics.setColor(0,0,0)
+	love.graphics.rectangle("fill",0,0,WIDTH,topY)
+	love.graphics.rectangle("fill",0,0,topX,HEIGHT)
+	love.graphics.rectangle("fill",topX+g_top:getWidth(),0,WIDTH-topX+g_top:getWidth(),HEIGHT)
+	love.graphics.rectangle("fill",0,(bottomY+g_bottom:getHeight()),WIDTH,HEIGHT - (bottomY+g_bottom:getHeight()))
+	
 end
 
 function drawLid(gfx, mouseX, mouseY, open, isTop)
 	yOffset = 250 * open
 	if isTop then yOffset = yOffset * -1 end
 	lidX = mouseX - gfx:getWidth() / 2
-	lidY = mouseY - gfx:getHeight() / 2
-	love.graphics.draw(gfx,lidX,lidY + yOffset)
+	lidY = (mouseY - gfx:getHeight() / 2) + yOffset
+	love.graphics.draw(gfx,lidX,lidY)
+	return lidX, lidY
 end
